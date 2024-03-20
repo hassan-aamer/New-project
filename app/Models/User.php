17f8406b8346c8107b23a95d207f20d144c6ef8e
hassan-aamer <hassanaamer048@gmail.com>
2024-03-20 15:58:52 +0200

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
